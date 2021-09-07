49bc39ad8fc2d0928eccee3e2d0cf11a04185580
mram0s <mariana-.ramos@hotmail.com>
2021-09-07 11:20:57 -0300

    fix conflicts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Technologies from "./pages/Technologies";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to Technology's CRUD</h1>
+      <Technologies />
+    </>
+  );
+}
+
+export default App;
